c1a048b208433be7d859da302fcffb0262f7e849
gungniir <vladimir.kurus@yandex.ru>
2021-10-08 21:01:49 +0700

    Fix Module 21 Task 1

--- Module 21/Task 1/main.go
+++ Module 21/Task 1/main.go
@@ -9,7 +9,5 @@ func main() {
 		z float32 = 1.2
 	)
 
-	var S = 2*float32(x) + float32(y)*float32(y) - 3/z
-
-	fmt.Println(S)
+	fmt.Println(func(x int16, y uint8, z float32) float32 { return 2*float32(x) + float32(y)*float32(y) - 3/z }(x, y, z))
 }
